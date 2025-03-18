b29a1c5c082dbb8091869887b06479cb5812d2ca
noook <me@neilrichter.com>
2025-03-18 16:24:14 +0100

    chore: remove exception handling (http error should not be caught)

--- src/Controller/ShortLinkController.php
+++ src/Controller/ShortLinkController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\DTO\CreateShortLinkDTO;
 use App\Service\ShortLinkService;
-use App\Exceptions\ShortLinkConflictException;
-use App\Exceptions\ShortLinkTooLongException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
@@ -20,21 +18,7 @@ final class ShortLinkController extends AbstractController
         ShortLinkService $shortLinkService,
     ): Response
     {
-        // a try block allows to catch errors that could occur during the execution of the code
-        try {
-            $shortLink = $shortLinkService->createShortLink($dto);
-            return $this->json($shortLink);
-        } catch (ShortLinkConflictException $e) {
-            // Here we are catching the exception of type ShortLinkConflictException. We can handle it here.
-            throw $e;
-        } catch(ShortLinkTooLongException $e) {
-            // Here we are catching the exception of type ShortLinkTooLongException. We can handle it here.
-        } catch (\Exception $e) {
-            // Here we are catching any other exception that might be thrown. We can handle it here.
-            throw $e;
-        }
-
-        // Make sure to return something. It could be an empty response or an Exception.
-        return $this->json([]);
+        $shortLink = $shortLinkService->createShortLink($dto);
+        return $this->json($shortLink);
     }
 }
